2591c215121615b783501b2dce3866136a7a8ee7
maninoj <maninajafi1384@gmail.com>
2024-11-01 16:01:59 +0330

    func find_parity done

--- haming code/main.py
+++ haming code/main.py
@@ -1,6 +1,17 @@
 class HammingCode:
     def __init__(self, code):
         self.code = code
-        self.parity_bits = [1, 2, 4, 8, 16]
+        self.parity_loc = [1, 2, 4, 8, 16]
 
-    
+    def find_parity(self):
+        bit_list = []
+        parity_bits = {}
+
+        for bit in self.code:
+            bit_list.append(int(bit))
+
+        for p in self.parity_loc:
+            parity_sum = sum(bit_list[i - 1] for i in range(1, len(bit_list) + 1) if i & p)
+            parity_bits[p] = parity_sum % 2
+
+        return parity_bits
